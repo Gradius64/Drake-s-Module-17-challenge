aa7ac2a005b610592c234b028f02907368577863
Draca Baugh-Flowers <66185683+Gradius64@users.noreply.github.com>
2024-12-11 19:30:28 -0600

    Add files via upload

---
+++ createdAt.ts
@@ -0,0 +1,14 @@
+
+
+/// this is a reaction body
+
+interface Reaction {
+        id: string;
+        userId: string;
+        postId: string;
+        type: ReactionType;
+        createdAt: Date;
+}
+
+// Create a username by combining parts of the first name, last name, and birth year
+
